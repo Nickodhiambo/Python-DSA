502cde3bd3a13fe99c91f04d07af31f0a39af8c7
Nick <nodhiambo01@gmail.com>
2024-10-07 15:44:43 +0300

    square and sum functions

---
+++ Functions/square_list.py
@@ -0,0 +1,9 @@
+def squareNums(nums):
+    return [x ** 2 for x in nums]
+
+def test():
+    list_of_numbers = [1, 2, 3, 4, 5]
+    squares = squareNums(list_of_numbers)
+    print(squares)
+
+test()
